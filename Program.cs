62bbfb52eb222f355dd4832bc657c9b82eeb0241
Caleb Roskelley <caleb.roskelley@students.snow.edu>
2025-01-23 00:31:57 -0700

    passes tests for non recursive sorting method

--- Program.cs
+++ Program.cs
@@ -4,17 +4,27 @@
     {
 
         Console.WriteLine("Hello! This program will take 2 lists of numbers and combine them in number order from least to greatest");
+        //test 1
+        System.Diagnostics.Debug.Assert(Enumerable.SequenceEqual(
+        mergeArrays(new int[]{1, 3, 5}, new int[]{-5, 3, 6, 7}),
+        new int[]{-5, 1, 3, 3, 5, 6, 7}));
+        Console.ReadKey();
+        //test 2
+        System.Diagnostics.Debug.Assert(Enumerable.SequenceEqual(
+        mergeArrays(new int[]{-5, 2, 5, 8, 10}, new int[]{1, 2, 5}),
+        new int[]{-5, 1, 2, 2, 5, 5, 8, 10}));
+        Console.ReadKey();
+        Console.WriteLine("Passed first 2 tests");
         Console.WriteLine("Please give the first list of numbers");
         int[] array1 = collectArrays();
         Console.WriteLine("Now give the second list of numbers");
         int[] array2 = collectArrays();
-        foreach(int num  in mergeArrays(array1, array2))
+        int[] combinedArray = mergeArrays(array1, array2);
+        foreach(int num  in combinedArray)
         {
             Console.Write(num + " ");
         }
-        System.Diagnostics.Debug.Assert(Enumerable.SequenceEqual)(
-        mergeArrays(new int[]{1, 3, 5}, new int[]{-5, 3, 6, 7}),
-        new int[]{-5, 1, 3, 3, 5, 6, 7});
+        Console.WriteLine();
 
     }
 
@@ -22,7 +32,7 @@
     {
         Random rand = new Random();
         int arraySize = rand.Next(10);
-        int[] intArray = new int[arraySize];
+        int[] intArray = new int[5];
         bool validAnswer = false;
         int num;
         for(int i = 0; i < intArray.Length; i++)
@@ -50,7 +60,8 @@
         int bIndex = 0;
         for(int i = 0; i < mergedList.Length; i++)
         {
-            if(bIndex == b.Length || (a[aIndex] < b[bIndex] && aIndex != a.Length))
+
+            if(bIndex == b.Length || ( aIndex < a.Length && a[aIndex] < b[bIndex]))
             {
                 mergedList[i] = a[aIndex];
                 aIndex++;
@@ -61,7 +72,6 @@
                 bIndex++;
             }
         }
-
         return mergedList;
     }
 }
